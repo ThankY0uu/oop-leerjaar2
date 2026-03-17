4a927682148b475e064c0cbc19fd7d61d56d3717
ThankY0u <1208522@student.roc-nijmegen.nl>
2026-03-17 13:43:52 +0100

    Skelet van de backend bestanden

---
+++ DEALER/classes/Deck.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DEALER.classes
+{
+    internal class Table
+    {
+    }
+}
---
+++ DEALER/classes/Shoe.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DEALER
+{
+    internal class Table
+    {
+    }
+}
